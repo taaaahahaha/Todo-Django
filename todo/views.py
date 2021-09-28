96bd679f194b2d41e9d4eda5dc2ff01e7e323cf1
Taaha Multani <taaha.m@somaiya.edu>
2021-09-29 00:43:08 +0530

    Initial commit

---
+++ todo/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from .models import Todo
+
+# Create your views here.
+def index(request):
+    todo=Todo.objects.all()
+    context = {
+        'todos':todo,
+    }
+
+    if request.method=='POST':
+        new_todo = Todo(title=request.POST['title'])
+        new_todo.save()
+        return redirect('/')
+
+    return render(request, 'index.html', context)
+
+def delete(request, pk):
+    todo = Todo.objects.get(id=pk)
+    todo.delete()
+    return redirect('/')
